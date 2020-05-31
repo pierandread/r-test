f743d3070dfe3bae567ddd59f20da9e827695ebe
Pier Andrea <pierandreadelise@gmail.com>
2020-05-31 19:55:07 +0200

    style: button in slider appear only on hover plus position

--- app/components/Slider/Slider.js
+++ app/components/Slider/Slider.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {getList} from '../../services/api-calls';
 import Movie from '../Movie/Movie';
+import './slider.css'
 
 
 function Slider ({list}) {
@@ -9,7 +10,7 @@ function Slider ({list}) {
   const [movingList, setMovingList] = useState(0);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   window.addEventListener("resize", function() {
-    setScreenWidth(window.innerWidth);
+    setScreenWidth(window.visualViewport.width || window.innerWidth);
   });
 
   const [listData, setListData] = useState(null);
@@ -18,15 +19,19 @@ function Slider ({list}) {
     getList(list).then(list=> setListData(list));
   },[])
 
+  
+
   return(
-    <div>
+    <div className="slider-div">
        {/* 280 from 250(width movie)+(10+10(paddings))+10(margin right) */}
-      <button onClick={()=> movingList!==0 && setMovingList(movingList+280)}>left</button>
+      <button className="button-left" onClick={()=> movingList!==0 && setMovingList(movingList+280)}>left</button>
+
       <div className='list' style={{"transform": `translateX(${movingList}px)`}}>
         {listData? listData.data.contents.data.map((movie,idx) => <Movie key={idx} movie={movie}/>) : <p>Loading...</p>}
       </div>
+
       {/* Math.floor(screenWidth/100*10) to consider margin div list-container + 30 to take out padding and margin right*/}
-      <button onClick={()=> {(-movingList<(((listData.data.contents.data.length)*280)-(screenWidth-(Math.floor(screenWidth/100*10))+30))) && setMovingList(movingList-280)}}>right</button>
+      <button className="button-right" style={{left: (screenWidth -(Math.floor(screenWidth/100*10)+42))}} onClick={()=> {(-movingList<(((listData.data.contents.data.length)*280)-(screenWidth-(Math.floor(screenWidth/100*10))+30))) && setMovingList(movingList-280)}}>right</button>
     </div>
   )
 }
