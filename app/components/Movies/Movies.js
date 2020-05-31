682b425a7f4875f549e96624702e86b1b98ec339
Pier Andrea <pierandreadelise@gmail.com>
2020-05-31 12:14:22 +0200

    split slider in another component

--- app/components/Movies/Movies.js
+++ app/components/Movies/Movies.js
@@ -1,61 +1,28 @@
 import React, {useState, useEffect} from 'react';
-import {getList} from '../../services/api-calls';
-import Movie from '../Movie/Movie';
+import Slider from '../Slider/Slider'
 import './movies.css'
 
 function Movies() {
 
-  //TODO: consider rejected promise for rendering, try to not reload if coming back to the page
-
-
-  // !(popularesList) && getList('populares-en-taquilla').then(list=> setPopularesList(list));
-
-  const [popularesList, setPopularesList] = useState(null)
-  const [estrenosFamiliaList, setEstrenosFamiliaList] = useState(null)
-  const [estrenosTaquillaList, setEstrenosTaquillaList] = useState(null)
-  const [estrenosEspanolesList, setEstrenosEspanolesList] = useState(null)
-  const [perdisteList, setPerdisteList] = useState(null)
-  const [xmenList, setXmenList] = useState(null)
-  const [semanaList, setSemanaList] = useState(null)
-  const [movingList, setMovingList] = useState(0)
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-
-  window.addEventListener("resize", function() {
-    setScreenWidth(window.innerWidth);
-  });
-
-  if(popularesList===null) {getList('populares-en-taquilla').then(list=> setPopularesList(list)); console.log("api CALL PD")};
 
   return (
     <div className='list-container' id='list-container'>
 
       <h3>Populares en Taquilla:</h3>
 
-      {/* left button 250 from 300+10+10 padding + 10 margin right  */}
-      <button onClick={()=> movingList!==0 && setMovingList(movingList+280)}>left</button>
-      
-      <div className='list' style={{"transform": `translateX(${movingList}px)`}}>
-        {popularesList? popularesList.data.contents.data.map((movie,idx) => <Movie key={idx} movie={movie}/>) : <p>Loading...</p>}
-      </div>
-      <button onClick={()=> {(-movingList<(((popularesList.data.contents.data.length)*280)-(screenWidth-(Math.floor(screenWidth/100*10))+20))) && setMovingList(movingList-280)}}>right</button>
+      <Slider list={'populares-en-taquilla'} />
 
-      <p>Estrenos para toda la familia:</p>
-      {estrenosFamiliaList? <p>Check list</p> : <p>Loading...</p>}
+      <h3>Estrenos para toda la familia:</h3>
 
-      <p>Estrenos imprescindibles en taquilla:</p>
-      {estrenosTaquillaList? <p>Check list</p> : <p>Loading...</p>}
+      <h3>Estrenos imprescindibles en taquilla:</h3>
 
-      <p>Estrenos espanoles:</p>
-      {estrenosEspanolesList? <p>Check list</p> : <p>Loading...</p>}
+      <h3>Estrenos espanoles:</h3>
 
-      <p>Si te perdiste:</p>
-      {perdisteList? <p>Check list</p> : <p>Loading...</p>}
+      <h3>Si te perdiste:</h3>
 
-      <p>Especial X-men:</p>
-      {xmenList? <p>Check list</p> : <p>Loading...</p>}
+      <h3>Especial X-men:</h3>
 
-      <p>Nuestras preferidas de la semana:</p>
-      {semanaList? <p>Check list</p> : <p>Loading...</p>}
+      <h3>Nuestras preferidas de la semana:</h3>
 
     </div>
   );
---
+++ app/components/Slider/Slider.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import {getList} from '../../services/api-calls';
+import Movie from '../Movie/Movie';
+
+
+function Slider ({list}) {
+
+  //states to manager left and rigth stopping points for slider
+  const [movingList, setMovingList] = useState(0);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  window.addEventListener("resize", function() {
+    setScreenWidth(window.innerWidth);
+  });
+
+  const [listData, setListData] = useState(null);
+
+  useEffect(() =>{
+    getList(list).then(list=> setListData(list)); console.log("api CALL PD")
+  },[])
+
+  return(
+    <div>
+       {/* 280 from 250(width movie)+(10+10(paddings))+10(margin right) */}
+      <button onClick={()=> movingList!==0 && setMovingList(movingList+280)}>left</button>
+      <div className='list' style={{"transform": `translateX(${movingList}px)`}}>
+        {listData? listData.data.contents.data.map((movie,idx) => <Movie key={idx} movie={movie}/>) : <p>Loading...</p>}
+      </div>
+      {/* Math.floor(screenWidth/100*10) to consider margin div list-container + 10 to take out last margin right*/}
+      <button onClick={()=> {(-movingList<(((listData.data.contents.data.length)*280)-(screenWidth-(Math.floor(screenWidth/100*10))+10))) && setMovingList(movingList-280)}}>right</button>
+    </div>
+  )
+}
+
+export default Slider;
